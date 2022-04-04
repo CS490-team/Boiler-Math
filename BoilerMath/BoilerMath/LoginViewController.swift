baf2f9a1b8f35100ed5c8f3e157d12427aa7aea6
rriecc <s715860428@gmail.com>
2022-04-04 17:06:55 -0400

    fix tap bug

--- BoilerMath/BoilerMath/LoginViewController.swift
+++ BoilerMath/BoilerMath/LoginViewController.swift
@@ -11,7 +11,7 @@ import Parse
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
+        tap.cancelsTouchesInView = true
         view.addGestureRecognizer(tap)
     }
     
